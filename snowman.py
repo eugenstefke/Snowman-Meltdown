934e81a8ad12c1a1062d95cf16ade15ab2b75b83
Eugen Stefke <eugen.stefke@web.de>
2026-07-14 10:22:19 +0200

    with game logic

--- snowman.py
+++ snowman.py
@@ -3,22 +3,68 @@ import random
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\
+     """
+ ]
 
 def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
+def display_game_state(mistakes, secret_word, guessed_letters):
+    print(STAGES[mistakes])
+    display_word = ""
+    for char in secret_word:
+        if char in guessed_letters:
+            display_word += char + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
+    print("\n")
 
 def play_game():
     secret_word = get_random_word()
+    mistakes = 0
+    guessed_letters = []
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
+
     while True:
-        # For now, simply prompt the user once:
+        display_game_state(mistakes, secret_word, guessed_letters)
+
         guess = input("Guess a letter: ").lower()
+        if guess in guessed_letters:
+            pass
+        else:
+            guessed_letters.append(guess)
         print("You guessed:", guess)
-        break
 
 
 if __name__ == "__main__":
